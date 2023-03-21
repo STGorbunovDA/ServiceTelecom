2b7d4b7003b8efbec92b44af8489c128af585ae7
STGorbunovDA <bous07@mail.ru>
2023-03-21 17:35:58 +0800

    #1

--- ServiceTelecom/View/LoginView.xaml.cs
+++ ServiceTelecom/View/LoginView.xaml.cs
@@ -30,14 +30,50 @@ namespace ServiceTelecom.View
                 DragMove();
         }
 
-        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
         {
-            WindowState= WindowState.Minimized;
+            WindowState = WindowState.Minimized;
         }
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void ChbPass_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)chbPass.IsChecked)
+            {
+                txbPass.Visibility = Visibility.Visible;
+                psbPass.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                txbPass.Visibility = Visibility.Collapsed;
+                psbPass.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void PsbPass_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (psbPass.Password.Length > 0)
+                txbPass.Text = psbPass.Password;
+        }
+
+        private void TxbPass_KeyUp(object sender, KeyEventArgs e)
+        {
+            psbPass.Password = txbPass.Text;
+        }
+
+        private void BtnAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TxbClearPassword_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            psbPass.Password = string.Empty;
+            txbPass.Text = string.Empty;
         }
     }
 }
