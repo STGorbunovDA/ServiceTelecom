80f16e3e81fa215d59d9f5187933bfbbae87b077
STGorbunovDA <bous07@mail.ru>
2023-03-22 20:03:41 +0800

    .

---
+++ ServiceTelecom/Repositories/RepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace ServiceTelecom.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+
+        public RepositoryBase()
+        {
+            _connectionString = "Server(local); Database=MVVMLoginDb; Integrated Security=true";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
