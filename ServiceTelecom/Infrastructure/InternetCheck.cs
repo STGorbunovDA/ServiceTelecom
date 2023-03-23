3de6bf85688888b735bd73decfebde3ab4e5da53
STGorbunovDA <bous07@mail.ru>
2023-03-23 19:34:25 +0800

    Содание метода проверки интернета

---
+++ ServiceTelecom/Infrastructure/InternetCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ServiceTelecom.Infrastructure
+{
+    internal class InternetCheck
+    {
+        /// <summary>
+        /// Проверка Интернета
+        /// </summary>
+        /// <returns></returns>
+        public static bool CheckSkyNET()
+        {
+            try
+            {
+                Dns.GetHostEntry("yandex.com");
+                return true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"Отсутствует подключение к Интернету. Проверьте настройки сети и повторите попытку",
+                        "Сеть недоступна");
+                return false;
+            }
+        }
+    }
+}
--- ServiceTelecom/Repositories/UserRepository.cs
+++ ServiceTelecom/Repositories/UserRepository.cs
@@ -12,11 +12,6 @@ namespace ServiceTelecom.Repositories
 {
     public class UserRepository : RepositoryBase, IUserRepository
     {
-        public void Add(UserModel userModel)
-        {
-            throw new NotImplementedException();
-        }
-
         public bool AuthenticateUser(NetworkCredential credential)
         {
             bool validUser;
@@ -27,9 +22,14 @@ namespace ServiceTelecom.Repositories
                 command.CommandText = "SELECT * FROM [User] WHERE username=@username and [password]=@password";
                 command.Parameters.Add("@username", SqlDbType.NVarChar).Value = credential.UserName;
                 command.Parameters.Add("@password", SqlDbType.NVarChar).Value = credential.Password;
-                validUser = command.ExecuteScalar() ==null? false: true;
+                validUser = command.ExecuteScalar() == null ? false : true;
             }
             return validUser;
+        }
+
+        public void Add(UserModel userModel)
+        {
+            throw new NotImplementedException();
         }
 
         public void Edit(UserModel userModel)
