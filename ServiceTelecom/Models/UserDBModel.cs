045317cdedba545ca0531bddd3ff2a4dc2e205f8
STGorbunovDA <bous07@mail.ru>
2023-04-02 20:04:40 +0800

    Описание модели для получения списка всех пользователей

---
+++ ServiceTelecom/Models/UserDBModel.cs
@@ -0,0 +1,30 @@
+﻿using ServiceTelecom.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTelecom.Models
+{
+    public class UserDBModel : ViewModelBase
+    {
+        private int _id;
+        private string _login;
+        private string _password;
+        private string _post;
+
+        public UserDBModel(int id, string login, string password, string post)
+        {
+            IdBase= id;
+            LoginBase= login;
+            PasswordBase= password;
+            PostBase= post;
+        }
+
+        public int IdBase { get => _id; set { _id = value; OnPropertyChanged(nameof(IdBase)); } }
+        public string LoginBase { get => _login; set { _login = value; OnPropertyChanged(nameof(LoginBase)); } }
+        public string PasswordBase { get => _password; set { _password = value; OnPropertyChanged(nameof(PasswordBase)); } }
+        public string PostBase { get => _post; set { _post = value; OnPropertyChanged(nameof(PostBase)); } }
+    }
+}
