c7e5d77c272f6b1786e2bcd6eedd1c0e86b00795
STGorbunovDA <bous07@mail.ru>
2023-04-10 20:49:48 +0800

    Creating a StaffRegistrationViewModel

---
+++ ServiceTelecom/ViewModels/StaffRegistrationViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ServiceTelecom.Models;
+using ServiceTelecom.Repositories;
+using System.Collections.ObjectModel;
+
+namespace ServiceTelecom.ViewModels
+{
+    internal class StaffRegistrationViewModel : ViewModelBase
+    {
+        private UserRepository userRepository;
+        public ObservableCollection<StaffRegistrationsDataBaseModel> StaffRegistrations { get; set; }
+
+        private StaffRegistrationsDataBaseModel _staffRegistration;
+
+        public StaffRegistrationsDataBaseModel SelectedStaffRegistration
+        {
+            get => _staffRegistration;
+            set
+            {
+                _staffRegistration = value;
+                OnPropertyChanged(nameof(SelectedStaffRegistration));
+                //if (_user != null)
+                //{
+                //    Id = _user.IdBase;
+                //    Login = _user.LoginBase;
+                //    Password = _user.PasswordBase;
+                //    Post = _user.PostBase;
+                //}
+            }
+        }
+        public StaffRegistrationViewModel()
+        {
+            userRepository = new UserRepository();
+            StaffRegistrations = new ObservableCollection<StaffRegistrationsDataBaseModel>();
+            StaffRegistrations = userRepository.GetStaffRegistrationDataBase(StaffRegistrations);
+
+        }
+    }
+}
