e8fee4ddf7e1e7a85bf8b49507f708f2bf416f3b
STGorbunovDA <bous07@mail.ru>
2023-04-13 22:53:12 +0800

    Creating the IStaffRegistrationRepository interface

--- ServiceTelecom/Repositories/Interfaces/IStaffRegistrationRepository.cs
+++ ServiceTelecom/Repositories/Interfaces/IStaffRegistrationRepository.cs
@@ -1,10 +1,5 @@
 ﻿using ServiceTelecom.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServiceTelecom.Repositories.Interfaces
 {
@@ -15,6 +10,43 @@ namespace ServiceTelecom.Repositories.Interfaces
         /// </summary>
         /// <param name="staffRegistrations"></param>
         /// <returns></returns>
-        ObservableCollection<StaffRegistrationsDataBaseModel> GetStaffRegistrationDataBase(ObservableCollection<StaffRegistrationsDataBaseModel> staffRegistrations);
+        ObservableCollection<StaffRegistrationsDataBaseModel> GetStaffRegistrationsDataBase(ObservableCollection<StaffRegistrationsDataBaseModel> staffRegistrations);
+
+        /// <summary>
+        /// Добавление бригады на участок дороги
+        /// </summary>
+        /// <param name="sectionForeman"></param>
+        /// <param name="engineer"></param>
+        /// <param name="attorney"></param>
+        /// <param name="road"></param>
+        /// <param name="numberPrintDocument"></param>
+        /// <param name="curator"></param>
+        /// <param name="radioCommunicationDirectorate"></param>
+        /// <returns></returns>
+        bool AddStaffRegistrationDataBase(string sectionForeman, string engineer, string attorney,
+           string road, string numberPrintDocument, string curator, string radioCommunicationDirectorate);
+        /// <summary>
+        /// Изменение характеристик бригад
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="sectionForeman"></param>
+        /// <param name="engineer"></param>
+        /// <param name="attorney"></param>
+        /// <param name="road"></param>
+        /// <param name="numberPrintDocument"></param>
+        /// <param name="curator"></param>
+        /// <param name="radioCommunicationDirectorate"></param>
+        /// <returns></returns>
+        bool ChangeStaffRegistrationDataBase(int id, string sectionForeman, string engineer, string attorney,
+           string road, string numberPrintDocument, string curator, string radioCommunicationDirectorate);
+
+        /// <summary>
+        /// Удаление бригады
+        /// </summary>
+        /// <param name="staffRegistrations"></param>
+        /// <returns></returns>
+        void DeleteStaffRegistrationsDataBase(int id);
+
+
     }
 }
