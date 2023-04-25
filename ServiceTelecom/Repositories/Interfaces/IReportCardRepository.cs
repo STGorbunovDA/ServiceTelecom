830846e040c9b35930be876450a52ec34c97959c
STGorbunovDA <bous07@mail.ru>
2023-04-25 22:08:07 +0300

    Implemented methods of interaction with ReportCartView: 1. ReportCardView - added controllers and grid 2. ReportCardViewModel - Collections, properties, and methods for interaction with ReportCardView are added IReportCardRepository - An interface has been created 4. ReportCardRepository - Methods for getting data into collections from databases were processed in ReportCardViewModel

---
+++ ServiceTelecom/Repositories/Interfaces/IReportCardRepository.cs
@@ -0,0 +1,40 @@
+﻿using ServiceTelecom.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTelecom.Repositories.Interfaces
+{
+    internal interface IReportCardRepository
+    {
+        /// <summary>
+        /// Получение табеля сотрудников
+        /// </summary>
+        /// <param name="reportCards"></param>
+        /// <returns></returns>
+        ObservableCollection<ReportCardsDataBaseModel> GetReportCardsDataBase(ObservableCollection<ReportCardsDataBaseModel> reportCards);
+        
+        /// <summary>
+        /// Получение общих дат входа пользователей программы
+        /// </summary>
+        /// <param name="dateTimeInputCollections"></param>
+        /// <returns></returns>
+        ObservableCollection<string> GetDateTimeInputCollectionsDataBase(ObservableCollection<string> dateTimeInputCollections);
+
+        /// <summary>
+        /// Получение пользователей табеля
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        ObservableCollection<string> GetAllUsersReportCardsDataBase(ObservableCollection<string> users);
+
+        /// <summary>
+        /// Удаление строки табеля сотрудника
+        /// </summary>
+        /// <param name="idReportCards"></param>
+        void DeleteReportCardsDataBase(int idReportCards);
+    }
+}
--- ServiceTelecom/ViewModels/ReportCardViewModel.cs
+++ ServiceTelecom/ViewModels/ReportCardViewModel.cs
@@ -1,13 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ServiceTelecom.Models;
+using ServiceTelecom.Repositories;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ServiceTelecom.ViewModels
 {
-    internal class ReportCardViewModel
+    internal class ReportCardViewModel : ViewModelBase
     {
+        private ReportCardRepository reportCardRepository;
+        private ReportCardsDataBaseModel _reportCard;
+        public ObservableCollection<ReportCardsDataBaseModel> ReportCards { get; set; }
+        public ObservableCollection<string> DateTimeInputCollections { get; set; }
+        public ObservableCollection<string> Users { get; set; }
 
+        private string _user;
+        private string _dateTimeInput;
+
+        public string User { get => _user; set { _user = value; OnPropertyChanged(nameof(User)); } }
+        public string DateTimeInput { get => _dateTimeInput; set { _dateTimeInput = value; OnPropertyChanged(nameof(DateTimeInput)); } }
+
+        public ReportCardsDataBaseModel SelectedReportCardViewModel
+        {
+            get => _reportCard;
+            set
+            {
+                _reportCard = value;
+                OnPropertyChanged(nameof(SelectedReportCardViewModel));
+                if (_reportCard != null)
+                {
+                    User = _reportCard.User;
+                    DateTimeInput = _reportCard.DateTimeInput.Remove(_reportCard.DateTimeInput.IndexOf(" "));
+                }
+            }
+        }
+
+        public ICommand UpdateReportCardsDataBase { get; }
+        public ICommand SaveReportCardsDataBase { get; }
+        public ICommand DeleteReportCardsDataBase { get; }
+
+        public ReportCardViewModel()
+        {
+            reportCardRepository = new ReportCardRepository();
+            ReportCards = new ObservableCollection<ReportCardsDataBaseModel>();
+            DateTimeInputCollections = new ObservableCollection<string>();
+            Users = new ObservableCollection<string>();
+            GetStaffReportCardsForUpdate();
+            UpdateReportCardsDataBase = new ViewModelCommand(ExecuteUpdateReportCardsDataBaseCommand);
+            DeleteReportCardsDataBase = new ViewModelCommand(ExecuteDeleteReportCardsDataBaseCommand);
+        }
+
+
+
+
+
+        #region DeleteReportCardsDataBase
+
+        private void ExecuteDeleteReportCardsDataBaseCommand(object obj)
+        {
+            GetStaffReportCardsForUpdate();
+        }
+
+        internal void GetAllSelectRowsReportCardsAndDeleteId(DataGrid datagrid)
+        {
+            if (datagrid.SelectedItems.Count > 0)
+                foreach (ReportCardsDataBaseModel _reportCard in datagrid.SelectedItems)
+                    reportCardRepository.DeleteReportCardsDataBase(_reportCard.IdReportCards);
+        }
+
+        #endregion
+
+        #region UpdateReportCardsDataBase 
+
+        private void ExecuteUpdateReportCardsDataBaseCommand(object obj)
+        {
+            GetStaffReportCardsForUpdate();
+        }
+
+        #endregion
+
+        /// <summary> Получить всё из БД </summary>
+        private void GetStaffReportCardsForUpdate()
+        {
+            if (ReportCards.Count != 0 || DateTimeInputCollections.Count != 0 || Users.Count != 0)
+            {
+                ReportCards.Clear();
+                DateTimeInputCollections.Clear();
+                Users.Clear();
+            }
+
+            ReportCards = reportCardRepository.GetReportCardsDataBase(ReportCards);
+            DateTimeInputCollections = reportCardRepository.GetDateTimeInputCollectionsDataBase(DateTimeInputCollections);
+            Users = reportCardRepository.GetAllUsersReportCardsDataBase(Users);
+        }
     }
 }
