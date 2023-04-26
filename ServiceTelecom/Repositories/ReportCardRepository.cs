d98a855e121db58ef7420811844509965b7ec441
STGorbunovDA <bous07@mail.ru>
2023-04-26 20:06:06 +0300

    Implementation of methods: Obtaining a timesheet for the selected user and Retrieving timesheets by login date implementation

--- ServiceTelecom/Repositories/ReportCardRepository.cs
+++ ServiceTelecom/Repositories/ReportCardRepository.cs
@@ -44,6 +44,7 @@ namespace ServiceTelecom.Repositories
             finally { RepositoryDataBase.GetInstance.CloseConnection(); }
         }
 
+
         public ObservableCollection<string> 
             GetDateTimeInputCollectionsDataBase(ObservableCollection<string> dateTimeInputCollections)
         {
@@ -73,34 +74,6 @@ namespace ServiceTelecom.Repositories
             finally { RepositoryDataBase.GetInstance.CloseConnection(); }
         }
 
-        public ObservableCollection<string> 
-            GetAllUsersReportCardsDataBase(ObservableCollection<string> users)
-        {
-            try
-            {
-                if (!InternetCheck.CheckSkyNET())
-                    return users;
-                using (MySqlCommand command = new MySqlCommand("GetAllUsersReportCardsDataBase",
-                    RepositoryDataBase.GetInstance.GetConnection()))
-                {
-                    RepositoryDataBase.GetInstance.OpenConnection();
-                    using (MySqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.HasRows)
-                        {
-                            while (reader.Read())
-                                users.Add(
-                                    Encryption.DecryptCipherTextToPlainText(reader.GetString(0)));
-                        }
-                        reader.Close();
-                        return users;
-                    }
-                }
-            }
-            catch (Exception) { return users; }
-            finally { RepositoryDataBase.GetInstance.CloseConnection(); }
-        }
-
         public void DeleteReportCardsDataBase(int idReportCards)
         {
             try
@@ -117,6 +90,81 @@ namespace ServiceTelecom.Repositories
                 }
             }
             catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+            finally { RepositoryDataBase.GetInstance.CloseConnection(); }
+        }
+
+        public ObservableCollection<ReportCardsDataBaseModel> 
+            GetReportCardsAtCmbUserDataBase(ObservableCollection<ReportCardsDataBaseModel> 
+            reportCards, string cmbUser)
+        {
+            try
+            {
+
+                if (!InternetCheck.CheckSkyNET())
+                    return reportCards;
+                using (MySqlCommand command = new MySqlCommand("GetReportCardsAtCmbUserDataBase",
+                    RepositoryDataBase.GetInstance.GetConnection()))
+                {
+                    RepositoryDataBase.GetInstance.OpenConnection();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue($"cmbUser", Encryption.EncryptPlainTextToCipherText(cmbUser));
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                ReportCardsDataBaseModel reportCard = new ReportCardsDataBaseModel(
+                                    reader.GetInt32(0),
+                                    Encryption.DecryptCipherTextToPlainText(reader.GetString(1)),
+                                    reader.GetDateTime(2), reader.GetDateTime(3));
+                                reportCards.Add(reportCard);
+                            }
+                        }
+                        reader.Close();
+                        return reportCards;
+                    }
+                }
+            }
+            catch (Exception) { return reportCards; }
+            finally { RepositoryDataBase.GetInstance.CloseConnection(); }
+        }
+
+
+        public ObservableCollection<ReportCardsDataBaseModel> 
+            GetReportCardsAtCmbDateTimeInput(ObservableCollection<ReportCardsDataBaseModel> reportCards, 
+            string selectedItemCmbUser)
+        {
+            try
+            {
+                if (!InternetCheck.CheckSkyNET())
+                    return reportCards;
+                using (MySqlCommand command = new MySqlCommand("GetReportCardsAtCmbDateTimeInput",
+                    RepositoryDataBase.GetInstance.GetConnection()))
+                {
+                    RepositoryDataBase.GetInstance.OpenConnection();
+                    string date = Convert.ToDateTime(selectedItemCmbUser).ToString("yyyy-MM-dd");
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue($"date", date);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                ReportCardsDataBaseModel reportCard = new ReportCardsDataBaseModel(
+                                    reader.GetInt32(0),
+                                    Encryption.DecryptCipherTextToPlainText(reader.GetString(1)),
+                                    reader.GetDateTime(2), reader.GetDateTime(3));
+                                reportCards.Add(reportCard);
+                            }
+                        }
+                        reader.Close();
+                        return reportCards;
+                    }
+                }
+            }
+            catch (Exception) { return reportCards; }
             finally { RepositoryDataBase.GetInstance.CloseConnection(); }
         }
     }
