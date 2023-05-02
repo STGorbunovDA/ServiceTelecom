ee578e9163c79514eefc239e50359034a979370f
STGorbunovDA <bous07@mail.ru>
2023-05-03 00:08:25 +0300

    Added method to get all radio models: ModelDataBase IModelDataBase

---
+++ ServiceTelecom/Repositories/Interfaces/IModelDataBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ServiceTelecom.Repositories.Interfaces
+{
+    public interface IModelDataBase
+    {
+        ObservableCollection<string> GetModelDataBase(ObservableCollection<string> modelCollections);
+    }
+}
