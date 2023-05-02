b042846a50f6c045622dc5552bed324619bf59a2
STGorbunovDA <bous07@mail.ru>
2023-05-03 00:06:01 +0300

    Creating a window to add a repair manual for an engineer: AddTutorialEngineerView AddTutorialEngineerViewModel

---
+++ ServiceTelecom/ViewModels/TutorialEngineerViewModelPackage/AddTutorialEngineerViewModel.cs
@@ -0,0 +1,45 @@
+﻿using ServiceTelecom.Repositories.Base;
+using ServiceTelecom.View.Base;
+using ServiceTelecom.View.TutorialEngineerViewPackage;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ServiceTelecom.ViewModels.TutorialEngineerViewModelPackage
+{
+    internal class AddTutorialEngineerViewModel :ViewModelBase
+    {
+        AddModelRadiostantionView addModelRadiostantion = null;
+        private ModelDataBase _modelDataBase;
+        public ObservableCollection<string> ModelCollections { get; }
+
+        private string _model;
+        public string Model { get => _model; set { _model = value; OnPropertyChanged(nameof(Model)); } }
+
+        public ICommand AddModelDataBase { get; }
+
+        public AddTutorialEngineerViewModel() 
+        {
+            ModelCollections = new ObservableCollection<string>();
+            _modelDataBase = new ModelDataBase();
+            ModelCollections = _modelDataBase.GetModelDataBase(ModelCollections);
+            AddModelDataBase = new ViewModelCommand(ExecuteAddModelDataBaseCommand);
+        }
+
+        #region Добавить модель радиостанции
+
+        private void ExecuteAddModelDataBaseCommand(object obj)
+        {
+            if (addModelRadiostantion == null)
+            {
+                addModelRadiostantion = new AddModelRadiostantionView();
+                addModelRadiostantion.Closed += (sender, args) => addModelRadiostantion = null;
+                addModelRadiostantion.Show();
+            }
+        }
+
+        #endregion
+
+
+    }
+}
