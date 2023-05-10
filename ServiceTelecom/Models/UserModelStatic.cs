9859c5d64e8a74134272269368ebfc92cbe74300
STGorbunovDA <bous07@mail.ru>
2023-05-10 22:58:55 +0300

    Temporary solution: implementation of adding a list of roads to the static collection to form a collection of radio stations based on the generated static collection of the list of roads

--- ServiceTelecom/Models/UserModelStatic.cs
+++ ServiceTelecom/Models/UserModelStatic.cs
@@ -1,16 +1,19 @@
 ﻿using ServiceTelecom.Infrastructure;
+using System.Collections.ObjectModel;
 
 namespace ServiceTelecom.Models
 {
     public class UserModelStatic
     {
+        //TODO решить проблему StaffRegistrationsDataBaseModelCollection
+        public static ObservableCollection<StaffRegistrationDataBaseModel> StaffRegistrationsDataBaseModelCollection { get; set; }
         public static string Login { get; private set; }
         public static string Post { get; private set; }//TODO продумать
         public UserModelStatic(string login, string post)
         {
             Login = Encryption.DecryptCipherTextToPlainText(login.Trim());
             Post = Encryption.DecryptCipherTextToPlainText(post);
-        }
-      
+            StaffRegistrationsDataBaseModelCollection = new ObservableCollection<StaffRegistrationDataBaseModel>();
+        }    
     }
 }
