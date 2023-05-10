f89b44ce147492829cc871cbae825c8b873177a1
STGorbunovDA <bous07@mail.ru>
2023-05-10 20:46:00 +0300

    Creating WorkView.xaml

---
+++ ServiceTelecom/View/WorkViewPackage/WorkView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ServiceTelecom.View.WorkViewPackage
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkView.xaml
+    /// </summary>
+    public partial class WorkView : Window
+    {
+        public WorkView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
