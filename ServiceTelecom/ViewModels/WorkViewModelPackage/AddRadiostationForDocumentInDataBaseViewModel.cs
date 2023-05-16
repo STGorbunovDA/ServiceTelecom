912cb9ee5f38740d1eb0144fc3922b0496669e1f
STGorbunovDA <bous07@mail.ru>
2023-05-16 12:50:28 +0300

    Adding the desired properties

--- ServiceTelecom/ViewModels/WorkViewModelPackage/AddRadiostationForDocumentInDataBaseViewModel.cs
+++ ServiceTelecom/ViewModels/WorkViewModelPackage/AddRadiostationForDocumentInDataBaseViewModel.cs
@@ -1,7 +1,67 @@
-﻿namespace ServiceTelecom.ViewModels.WorkViewModelPackage
-{
-    internal class AddRadiostationForDocumentInDataBaseViewModel
-    {
+﻿using ServiceTelecom.Models;
+using ServiceTelecom.Repositories.Base;
+using ServiceTelecom.View.Base;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
+namespace ServiceTelecom.ViewModels.WorkViewModelPackage
+{
+    internal class AddRadiostationForDocumentInDataBaseViewModel : ViewModelBase
+    {
+        AddModelRadiostantionView addModelRadiostantion = null;
+        private ModelDataBaseRepository _modelDataBase;
+
+        public ObservableCollection<ModelRadiostantionDataBaseModel> ModelCollections { get; set; }
+
+        private string _model;
+        public string Model { get => _model; set { _model = value; OnPropertyChanged(nameof(Model)); } }
+
+        private int _theIndexModelChoiceCollection;
+        public int TheIndexModelChoiceCollection
+        {
+            get
+            {
+                return _theIndexModelChoiceCollection;
+            }
+            set
+            {
+                _theIndexModelChoiceCollection = value;
+                OnPropertyChanged(nameof(TheIndexModelChoiceCollection));
+            }
+        }
+        public ICommand AddModelDataBase { get; }
+
+        public AddRadiostationForDocumentInDataBaseViewModel()
+        {
+            ModelCollections = new ObservableCollection<ModelRadiostantionDataBaseModel>();
+            _modelDataBase = new ModelDataBaseRepository();
+            AddModelDataBase = new ViewModelCommand(ExecuteAddModelDataBaseCommand);
+            GetModelDataBase();
+        }
+
+        #region AddModelDataBase
+
+        private void ExecuteAddModelDataBaseCommand(object obj)
+        {
+            if (addModelRadiostantion == null)
+            {
+                addModelRadiostantion = new AddModelRadiostantionView();
+                addModelRadiostantion.Closed += (sender, args) => addModelRadiostantion = null;
+                addModelRadiostantion.Closed += (sender, args) => GetModelDataBase();
+                addModelRadiostantion.Show();
+            }
+        }
+
+        #endregion
+
+        private void GetModelDataBase()
+        {
+            TheIndexModelChoiceCollection = -1;
+            if (ModelCollections.Count != 0)
+                ModelCollections.Clear();
+            ModelCollections = _modelDataBase.GetModelRadiostantionDataBase(ModelCollections);
+            TheIndexModelChoiceCollection = ModelCollections.Count - 1;
+        }
     }
 }
