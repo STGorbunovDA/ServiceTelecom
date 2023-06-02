717706746180639a57214ea4dec358dbc44915ec
STGorbunovDA <bous07@mail.ru>
2023-06-03 00:35:49 +0800

    change WorkRepositoryRadiostantion

--- ServiceTelecom/Repositories/WorkRepositoryRadiostantion.cs
+++ ServiceTelecom/Repositories/WorkRepositoryRadiostantion.cs
@@ -529,48 +529,6 @@ namespace ServiceTelecom.Repositories
             finally { RepositoryDataBase.GetInstance.CloseConnection(); }
         }
 
-        public ObservableCollection<RepairManualRadiostantion>
-            GetRepairManualRadiostantionsCollections(
-            ObservableCollection<RepairManualRadiostantion>
-            repairManualRadiostantionsCollections, string model)
-        {
-            try
-            {
-                if (!InternetCheck.CheckSkyNET())
-                    return repairManualRadiostantionsCollections;
-                using (MySqlCommand command = new MySqlCommand(
-                   "GetRepairManualRadiostantionsCollections",
-                RepositoryDataBase.GetInstance.GetConnection()))
-                {
-                    RepositoryDataBase.GetInstance.OpenConnection();
-                    command.CommandType = CommandType.StoredProcedure;
-                    command.Parameters.AddWithValue($"modelUser",
-                        Encryption.EncryptPlainTextToCipherText(model));
-                    using (MySqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.HasRows)
-                        {
-                            while (reader.Read())
-                            {
-                                RepairManualRadiostantion
-                                    repairManualRadiostantion =
-                                    new RepairManualRadiostantion(
-                                    reader.GetInt32(0),
-                                    reader.GetString(1),
-                                    reader.GetString(2),
-                                    reader.GetString(3));
-                                repairManualRadiostantionsCollections.Add(
-                                    repairManualRadiostantion);
-                            }
-                        }
-                        reader.Close();
-                        return repairManualRadiostantionsCollections;
-                    }
-                }
-
-            }
-            catch { return repairManualRadiostantionsCollections; }
-            finally { RepositoryDataBase.GetInstance.CloseConnection(); }
-        }
+        
     }
 }
