10e638008616c763664e12d7bd28c874306210f0
STGorbunovDA <bous07@mail.ru>
2023-06-03 00:36:18 +0800

    Add RepairManualView

---
+++ ServiceTelecom/View/Base/RepairManualView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ServiceTelecom.View.Base
+{
+    /// <summary>
+    /// Логика взаимодействия для RepairManualView.xaml
+    /// </summary>
+    public partial class RepairManualView : Window
+    {
+        public RepairManualView(string model)
+        {
+            InitializeComponent();
+            txtModel.Text = model;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
