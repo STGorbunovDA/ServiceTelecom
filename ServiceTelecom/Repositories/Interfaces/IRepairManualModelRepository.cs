69613c0d222e64d5aeea2f65ba8a10eb45baa8c6
STGorbunovDA <bous07@mail.ru>
2023-06-03 00:35:27 +0800

    Add RepairManualModelRepository

---
+++ ServiceTelecom/Repositories/Interfaces/IRepairManualModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using ServiceTelecom.Models;
+using System.Collections.ObjectModel;
+
+namespace ServiceTelecom.Repositories.Interfaces
+{
+    internal interface IRepairManualModelRepository
+    {
+        /// <summary> Получение справочника ремонтов радиостанций по модели </summary>
+        ObservableCollection<RepairManualRadiostantion>
+            GetRepairManualRadiostantionsCollections(
+            ObservableCollection<RepairManualRadiostantion>
+            repairManualRadiostantionsCollections, string model);
+    }
+}
