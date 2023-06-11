79166354233643c5b55c2de59888d70ee013faf8
STGorbunovDA <bous07@mail.ru>
2023-06-12 05:29:55 +0800

    Add PrintRepairView and PrintRepairViewModel

---
+++ ServiceTelecom/View/WorkViewPackage/PrintRepairView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ServiceTelecom.View.WorkViewPackage
+{
+    /// <summary>
+    /// Логика взаимодействия для PrintRepairView.xaml
+    /// </summary>
+    public partial class PrintRepairView : Window
+    {
+        public PrintRepairView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
