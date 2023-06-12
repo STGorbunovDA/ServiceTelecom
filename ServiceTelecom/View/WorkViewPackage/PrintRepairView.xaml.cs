77c37ef454606f58071031d56eab7569eab0177a
STGorbunovDA <bous07@mail.ru>
2023-06-12 17:07:40 +0800

    Creating a View to add information about the company and its implementation of all methods for loading data into controls

--- ServiceTelecom/View/WorkViewPackage/PrintRepairView.xaml.cs
+++ ServiceTelecom/View/WorkViewPackage/PrintRepairView.xaml.cs
@@ -1,16 +1,54 @@
-﻿using System.Windows;
+﻿using ServiceTelecom.Infrastructure;
+using ServiceTelecom.Infrastructure.Interfaces;
+using ServiceTelecom.Models;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Input;
 
 namespace ServiceTelecom.View.WorkViewPackage
 {
-    /// <summary>
-    /// Логика взаимодействия для PrintRepairView.xaml
-    /// </summary>
     public partial class PrintRepairView : Window
     {
+        List<RepairDataCompanyModel> repairData;
+        GetSetRegistryServiceTelecomSetting getSetRegistryServiceTelecomSetting;
         public PrintRepairView()
         {
+            repairData = new List<RepairDataCompanyModel>();
+            getSetRegistryServiceTelecomSetting = new GetSetRegistryServiceTelecomSetting();
+            
             InitializeComponent();
+            
+            repairData = getSetRegistryServiceTelecomSetting.GetRepairData(txbCompany.Text);
+
+            foreach (var item in repairData)
+            {
+                txbOKPO.Text = item.OKPO;
+                txbBE.Text = item.BE;
+                txbFullNameCompany.Text = item.FullNameCompany;
+                txbChiefСompanyFIO.Text = item.ChiefСompanyFIO;
+                txbChiefСompanyPost.Text = item.ChiefСompanyPost;
+                txbChairmanСompanyFIO.Text = item.ChairmanСompanyFIO;
+                txbChairmanСompanyPost.Text = item.ChairmanСompanyPost;
+                txbFirstMemberCommissionFIO.Text = item.FirstMemberCommissionFIO;
+                txbFirstMemberCommissionPost.Text = item.FirstMemberCommissionPost;
+                txbSecondMemberCommissionFIO.Text = item.SecondMemberCommissionFIO;
+                txbSecondMemberCommissionPost.Text = item.SecondMemberCommissionPost;
+                txbThirdMemberCommissionFIO.Text = item.ThirdMemberCommissionFIO;
+                txbThirdMemberCommissionPost.Text = item.ThirdMemberCommissionPost;
+            }
+
+            if(!string.IsNullOrWhiteSpace(txbOKPO.Text) &&
+               !string.IsNullOrWhiteSpace(txbBE.Text) &&
+               !string.IsNullOrWhiteSpace(txbFullNameCompany.Text) &&
+               !string.IsNullOrWhiteSpace(txbChiefСompanyFIO.Text) &&
+               !string.IsNullOrWhiteSpace(txbChiefСompanyPost.Text) &&
+               !string.IsNullOrWhiteSpace(txbChairmanСompanyFIO.Text) &&
+               !string.IsNullOrWhiteSpace(txbChairmanСompanyPost.Text) &&
+               !string.IsNullOrWhiteSpace(txbFirstMemberCommissionFIO.Text) &&
+               !string.IsNullOrWhiteSpace(txbFirstMemberCommissionPost.Text) &&
+               !string.IsNullOrWhiteSpace(txbSecondMemberCommissionFIO.Text) &&
+               !string.IsNullOrWhiteSpace(txbSecondMemberCommissionPost.Text))
+                CheckContinue.IsChecked = true;
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
