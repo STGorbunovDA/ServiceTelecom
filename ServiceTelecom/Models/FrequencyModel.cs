a106b1eb55c3c45c87e0c22877b11de1df78270e
STGorbunovDA <bous07@mail.ru>
2023-06-17 17:12:33 +0800

    Add FrequencyModel

---
+++ ServiceTelecom/Models/FrequencyModel.cs
@@ -0,0 +1,25 @@
+﻿using ServiceTelecom.Infrastructure;
+using ServiceTelecom.ViewModels;
+
+namespace ServiceTelecom.Models
+{
+    internal class FrequencyModel : ViewModelBase
+    {
+        private int _id;
+        private string _frequency;
+
+        public int IdBase { get => _id; set { _id = value; OnPropertyChanged(nameof(IdBase)); } }
+        public string Frequency { get => _frequency; set { _frequency = value; OnPropertyChanged(nameof(Frequency)); } }
+
+        public FrequencyModel(int idBase, string frequency)
+        {
+            IdBase = idBase;
+            Frequency = Encryption.DecryptCipherTextToPlainText(Frequency);
+        }
+
+        public override string ToString()
+        {
+            return $"{Frequency}";
+        }
+    }
+}
