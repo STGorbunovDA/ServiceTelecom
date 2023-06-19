2e6a8feb5314d63467779f7b7d801c9e0f6109d8
STGorbunovDA <bous07@mail.ru>
2023-06-19 15:13:27 +0800

    Implemented the method of getting the parameter reference on the model

---
+++ ServiceTelecom/Repositories/Interfaces/IHandbookParametersModelRadiostationRepository.cs
@@ -0,0 +1,14 @@
+﻿using ServiceTelecom.Models;
+using System.Collections.ObjectModel;
+
+namespace ServiceTelecom.Repositories.Interfaces
+{
+    internal interface IHandbookParametersModelRadiostationRepository
+    {
+        /// <summary> Получение справочника параметров на модель </summary>
+        ObservableCollection<HandbookParametersModelRadiostationModel>
+            GetHandbookParametersAtModelForCollection(
+            ObservableCollection<HandbookParametersModelRadiostationModel>
+            handbookParametersModelRadiostationCollection, string model);
+    }
+}
