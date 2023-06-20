f2847ba56112637ba1db340acb360f5e6d693ad9
STGorbunovDA <bous07@mail.ru>
2023-06-20 15:40:00 +0800

    Temporary AddHandbookParametersViewModel

--- ServiceTelecom/ViewModels/Base/AddHandbookParametersViewModel.cs
+++ ServiceTelecom/ViewModels/Base/AddHandbookParametersViewModel.cs
@@ -1,7 +1,59 @@
-﻿namespace ServiceTelecom.ViewModels.Base
-{
-    internal class AddHandbookParametersViewModel
-    {
+﻿using ServiceTelecom.Models;
+using ServiceTelecom.Repositories;
+using System.Collections;
+using System.Collections.ObjectModel;
 
+namespace ServiceTelecom.ViewModels.Base
+{
+    internal class AddHandbookParametersViewModel : ViewModelBase
+    {
+        HandbookParametersModelRadiostationRepository _handbookParametersModelRadiostationRepository;
+
+        public ObservableCollection<HandbookParametersModelRadiostationModel>
+           HandbookParametersAllModelRadiostationCollection
+        { get; set; }
+
+        private int _selectedIndexHandbookParametersModel;
+        public int SelectedIndexHandbookParametersModel
+        {
+            get => _selectedIndexHandbookParametersModel;
+            set
+            {
+                _selectedIndexHandbookParametersModel = value;
+                OnPropertyChanged(nameof(SelectedIndexHandbookParametersModel));
+            }
+        }
+
+        private IList _selectedModels = new ArrayList();
+        public IList HandbookParametersModelMulipleSelectedDataGrid
+        {
+            get => _selectedModels;
+            set
+            {
+                _selectedModels = value;
+                OnPropertyChanged(nameof(HandbookParametersModelMulipleSelectedDataGrid));
+            }
+        }
+
+        HandbookParametersModelRadiostationModel _selectedHandbookParametersModel;
+        public HandbookParametersModelRadiostationModel SelectedHandbookParametersModel
+        {
+            get => _selectedHandbookParametersModel;
+            set
+            {
+                if (value == null)
+                    return;
+                _selectedHandbookParametersModel = value;
+                OnPropertyChanged(nameof(SelectedHandbookParametersModel));
+            }
+        }
+
+        public AddHandbookParametersViewModel()
+        {
+            _handbookParametersModelRadiostationRepository 
+                = new HandbookParametersModelRadiostationRepository();
+            HandbookParametersAllModelRadiostationCollection = 
+                new ObservableCollection<HandbookParametersModelRadiostationModel>();
+        }
     }
 }
