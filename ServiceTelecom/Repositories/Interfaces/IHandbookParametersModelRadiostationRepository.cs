f80968a01f5ea25c69edc41ab8654c29fd752706
STGorbunovDA <bous07@mail.ru>
2023-06-21 15:54:50 +0800

    Adding the parameter guide for the selected model type

--- ServiceTelecom/Repositories/Interfaces/IHandbookParametersModelRadiostationRepository.cs
+++ ServiceTelecom/Repositories/Interfaces/IHandbookParametersModelRadiostationRepository.cs
@@ -16,5 +16,25 @@ namespace ServiceTelecom.Repositories.Interfaces
             GetHandbookParametersAllModelForCollection(
             ObservableCollection<HandbookParametersModelRadiostationModel>
             handbookParametersModelCollection);
+
+        /// <summary> добавление справочника параметров для выбранного типа модели </summary>
+        bool AddHandbookParametersForModel(
+            string model, string minLowPowerLevelTransmitter,
+            string maxLowPowerLevelTransmitter, string minHighPowerLevelTransmitter,
+            string maxHighPowerLevelTransmitter, string minFrequencyDeviationTransmitter,
+            string maxFrequencyDeviationTransmitter, string minSensitivityTransmitter,
+            string maxSensitivityTransmitter, string minKNITransmitter,
+            string maxKNITransmitter, string minDeviationTransmitter,
+            string maxDeviationTransmitter, string minOutputPowerVoltReceiver,
+            string maxOutputPowerVoltReceiver, string minOutputPowerWattReceiver,
+            string maxOutputPowerWattReceiver, string minSelectivityReceiver,
+            string maxSelectivityReceiver, string minSensitivityReceiver,
+            string maxSensitivityReceiver, string minKNIReceiver, string maxKNIReceiver,
+            string minSuppressorReceiver, string maxSuppressorReceiver,
+            string minStandbyModeCurrentConsumption, string maxStandbyModeCurrentConsumption,
+            string minReceptionModeCurrentConsumption, string maxReceptionModeCurrentConsumption,
+            string minTransmissionModeCurrentConsumption, string maxTransmissionModeCurrentConsumption,
+            string minBatteryDischargeAlarmCurrentConsumption,
+            string maxBatteryDischargeAlarmCurrentConsumption);
     }
 }
