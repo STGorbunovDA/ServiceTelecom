aef4d2bbc9ec0e332267320d242447b926e3f988
STGorbunovDA <bous07@mail.ru>
2023-06-25 18:54:06 +0800

    build

--- ServiceTelecom/App.xaml.cs
+++ ServiceTelecom/App.xaml.cs
@@ -1,36 +1,28 @@
 ﻿using System.Windows;
 using System;
 using ServiceTelecom.View;
+using System.Threading;
 using ServiceTelecom.View.WorkViewPackage;
+using ServiceTelecom.Infrastructure;
 
 namespace ServiceTelecom
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
         [Obsolete]
-        protected void ApplicationStart(object sender, StartupEventArgs e)
+        private void Application_Startup(object sender, StartupEventArgs e)
         {
-            //TODO  разобраться с конфигом
-            //StaticConfig.userName = ConfigurationSettings.AppSettings["userName"];
-            //StaticConfig.password = ConfigurationSettings.AppSettings["password"];
-            //StaticConfig.dataBase = ConfigurationSettings.AppSettings["dataBase"];
-            //StaticConfig.word = ConfigurationSettings.AppSettings["word"];
-
-
-            //var addRadiostationParametersView = new AddRadiostationParametersView();
-            //addRadiostationParametersView.Show();
-
-            var loginView = new LoginView();
-            loginView.Show();
-
-            loginView.IsVisibleChanged += (s, ev) =>
+            if (InstanceChecker.TakeMemory())
             {
-                if (loginView.IsVisible == false && loginView.IsLoaded)
-                    loginView.Close();
-            };
+                var loginView = new LoginView();
+                loginView.Show();
+
+                loginView.IsVisibleChanged += (s, ev) =>
+                {
+                    if (loginView.IsVisible == false && loginView.IsLoaded)
+                        loginView.Close();
+                };
+            }        
         }
     }
 }
