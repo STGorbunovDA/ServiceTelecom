300f8439af8a5fb38aa3af3511a7e50b21c4045d
STGorbunovDA <bous07@mail.ru>
2023-06-25 12:37:31 +0800

    Add PrintReportsViewModel

---
+++ ServiceTelecom/ViewModels/WorkViewModelPackage/PrintReportsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ServiceTelecom.Infrastructure;
+using System;
+using System.Threading;
+using System.Windows.Input;
+
+namespace ServiceTelecom.ViewModels.WorkViewModelPackage
+{
+    internal class PrintReportsViewModel
+    {
+        private Print printExcel;
+        public ICommand PrintReportGeneralAKB { get; }
+        public PrintReportsViewModel()
+        {
+            printExcel = new Print();
+            PrintReportGeneralAKB = new ViewModelCommand(ExecutePrintReportGeneralCommand);
+        }
+
+        #region PrintReportGeneral
+
+        private void ExecutePrintReportGeneralCommand(object obj)
+        {
+            //printExcel.PrintReportGeneralAKB();
+            new Thread(() =>
+            {
+                printExcel.PrintReportGeneralAKB();
+            })
+            { IsBackground = true }.Start();
+        }
+
+        #endregion
+    }
+}
